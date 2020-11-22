80dc445386885b79952a39de40b4b96078f14e32
Utkarsh Kumar Singh <uks1k999@gmail.com>
2020-11-22 21:46:09 +0530

    add stationarity test

--- src/script.py
+++ src/script.py
@@ -1,34 +1,70 @@
-import pandas as pd
-import numpy as np
-from statsmodels.tsa.arima_model import ARIMA as arima
+import argparse
 from math import sqrt
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error as mse
+from statsmodels.tsa.arima_model import ARIMA as arima
+from statsmodels.tsa.stattools import adfuller, kpss
 
-filepath = '2010.csv'
-df = pd.read_csv(filepath)
+import warnings
+warnings.filterwarnings("ignore")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--filepath', required=True, help='input dataset')
+parser.add_argument('--lag_order', required=False, default=0, help='Number of lag observations(AR)')
+parser.add_argument('--diff_degree', required=False, default=0, help='Degree of differencing(I)')
+parser.add_argument('--ma_order', required=False, default=1, help='Moving average window size(MA)')
+params = parser.parse_args()
+
+lag_order = int(params.lag_order)
+diff_degree = int(params.diff_degree)
+ma_order = int(params.ma_order)
+
+# load dataframe
+df = pd.read_csv(params.filepath)
 df = df['Wind Speed']
-print(df.head())
+# print(df.head())
 x = df.to_numpy().tolist()
 
+# check if data is stationary
+print("AD Fuller Test")
+result = adfuller(x)
+print(f"ADF Statistic: {result[0]}")
+print(f"p-value: {result[1]}")
+print("Critical Values:")
+for k, v in result[4].items():
+    print(f"\t{k}: {v}")
+
+print("KPSS Test")
+result = kpss(x)
+print(f"KPSS Statistic: {result[0]}")
+print(f"p-value: {result[1]}")
+print("Critical Values:")
+for k, v in result[3].items():
+    print(f"\t{k}: {v}")
+
+# separate out validation set
 train_size = int(len(x)*0.8)
-train, test = x[:train_size], x[train_size:]
-print(len(train), len(test))
+train, val = x[:train_size], x[train_size:]
+# print(len(train), len(val))
 history = train
 preds = list()
 
-for i in range(len(test)):
-    model = arima(history, order=(0, 0, 1))
+# forecasting
+for i in range(len(val)):
+    model = arima(history, order=(lag_order, diff_degree, ma_order))
     model_fit = model.fit(disp=False)
     yhat = model_fit.forecast()[0]
     preds.append(yhat)
-    history.append(test[i])
-    # print(f'Predicted = {yhat}; Expected = {test[i]}')
+    history.append(val[i])
+    # print(f'Predicted = {yhat}; Expected = {val[i]}')
 
 print(model_fit.summary())
-rmse = sqrt(mse(test, preds))
+rmse = sqrt(mse(val, preds))
+print(f"RMSE: {rmse}")
 
-plt.plot(test, color='blue', label='actual')
+plt.plot(val, color='blue', label='actual')
 plt.plot(preds, color='red', label='prediction')
 plt.legend(loc="upper left")
 plt.show()
