b9cd6fc25623cca1faf143f1822768303e7648fd
Divyansh Agarwal <divyansh1199@gmail.com>
2020-11-22 20:47:27 +0530

    Update visualise.y

--- src/visualise.py
+++ src/visualise.py
@@ -2,4 +2,13 @@
     Script to compute and plot various descriptive statistics for the wind energy 
     time series data located in the 'data' folder in this repository.
 """
-import pandas
+import pandas as pd 
+from matplotlib import pyplot as plt
+
+pd.read_csv("data/TN.csv")
+plt.style.use("seaborn")
+
+
+
+plt.tight_layout()
+plt.show()
