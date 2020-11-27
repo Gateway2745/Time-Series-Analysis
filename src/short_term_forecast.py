448aa51582276a6d1669cb794b04cfe2082d66da
Gauransh <gauransh30@gmail.com>
2020-11-27 22:34:43 +0530

    Added Sarima code

--- src/short_term_forecast.py
+++ src/short_term_forecast.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error as mse
 
 from statsmodels.tsa.arima_model import ARIMA as arima
+from statsmodels.tsa.statespace.sarimax import SARIMAX as sarima
 from statsmodels.tsa.stattools import adfuller, kpss
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
@@ -31,7 +32,7 @@ ma_order = int(params.ma_order)
 df = pd.read_csv(params.filepath)
 
 # separate out validation set
-train_data = df.loc[(df['Year'] == 2013) & (df['Month']==12)]['Wind Speed'].to_numpy().tolist()
+train_data = df.loc[(df['Year'] == 2013)]['Wind Speed'].to_numpy().tolist()
 val_data = df.loc[(df['Year'] == 2014) & (df['Month']==1)]['Wind Speed'].to_numpy().tolist()
 train_size = len(train_data)
 val_size = len(val_data)
@@ -40,21 +41,29 @@ history = train_data
 preds = list()
 
 # forecasting
-for i in range(len(val_data)):
-    model = arima(history, order=(lag_order, diff_degree, ma_order))
-    model_fit = model.fit(disp=False)
-    yhat = model_fit.forecast()[0]
-    preds.append(yhat)
-    history.append(val_data[i])
+# for i in range(len(val_data)):
+#     model = arima(history, order=(lag_order, diff_degree, ma_order))
+#     model_fit = model.fit(disp=False)
+#     yhat = model_fit.forecast()[0]
+#     preds.append(yhat)
+#     history.append(val_data[i])
     # print(f'Predicted = {yhat}; Expected = {val[i]}')
 
+model = sarima(history, order=(lag_order, diff_degree, ma_order), seasonal_order=(0, 1, 1, 24))
+model_fit = model.fit(disp=False)
+yhat = model_fit.forecast(steps=744)
+#preds.append(yhat)
+#history.append(val_data[i])
+
 print(model_fit.summary())
-rmse = sqrt(mse(val_data, preds))
+rmse = sqrt(mse(val_data, yhat))
 print(f"RMSE: {rmse}")
 
 plt.figure()
-plt.plot(val_data[:48], color='blue', label='actual')
-plt.plot(preds[:48], color='red', label='prediction')
+plt.plot(val_data, color='blue', label='actual')
+plt.plot(yhat, color='red', label='prediction')
+plt.xlabel('Hour')
+plt.ylabel('WindSpeed')
 plt.legend(loc="upper left")
-plt.savefig('forecast_results/AP/48hrs_arma.png')
+plt.savefig('../forecast_results/TN/48hrs_sarima.png')
 plt.close()
